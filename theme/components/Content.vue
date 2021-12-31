97a40ae08f64e2790c6689a7cdb1f1109df313cb
pengqiang <839556387@qq.com>
2021-12-31 13:48:17 +0800

    add Spin Component

--- theme/components/Content.vue
+++ theme/components/Content.vue
@@ -1,17 +1,27 @@
-<template functional>
+<template>
 <el-row>
     <el-col 
     :span="24"
-    :style="{'min-height': props.startMinHeight ? props.minHeight : ''}" 
-    :class="['content-box', {'content-box-min-height': props.startMinHeight}]">
-        <slot></slot>
+    :style="{'min-height': startMinHeight ? minHeight : ''}" 
+    :class="['content-box', {'content-box-min-height': startMinHeight}]">
+        <slot name="tool"></slot>
+        <div :class="['content-table-box', { 'content-table-loading': loading }]">
+            <slot></slot>
+            <div v-if="loading" class="spin-box">
+                <Spin />
+            </div>
+        </div>
     </el-col>
 </el-row>
 </template>
 
 <script>
+import Spin from './Spin'
 export default {
     name: 'Content',
+    components: {
+        Spin
+    },
     props: {
         startMinHeight: {
             type: Boolean,
@@ -20,6 +30,10 @@ export default {
         minHeight: {
             type: String,
             default: '500px'
+        },
+        loading: {
+            type: Boolean,
+            default: false
         }
     }
 }
@@ -30,6 +44,12 @@ export default {
     padding: 24px;
     background: #fff;
 }
+.content-table-box {
+    position: relative;
+    &.content-table-loading {
+        opacity: .5;
+    }
+}
 .content-box-min-height {
     .el-dialog__wrapper{
         overflow: hidden;
@@ -39,5 +59,15 @@ export default {
         overflow-y: auto;
         box-sizing: border-box;
     }
+}
+.spin-box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
