efec4c832d138cc5defb82bbdf379b4049b876d1
pengqiang <839556387@qq.com>
2022-03-22 15:50:42 +0800

    update pagination

--- theme/components/Pagination.vue
+++ theme/components/Pagination.vue
@@ -1,5 +1,6 @@
 <template functional>
-<el-row>
+<el-row class="pagination-box">
+    <span v-if="!props.hideTotal && props.total" class="pagination-number">{{ (props.current - 1) * props.pageSize + 1 }}-{{ props.current * props.pageSize > props.total ? props.total : props.current * props.pageSize }}</span>
     <slot 
     :sizes="[10, 20, 30, 40]"
     layout="total, prev, pager, next, sizes"
@@ -14,5 +15,16 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-  
+.pagination-box {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    .pagination-number {
+        line-height: 28px;
+        margin-right: 2px;
+        display: block;
+        color:#606266;
+        font-size: 13px;
+    }
+} 
 </style>
